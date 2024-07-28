4fdb2f647f41e734a701ed560bf448455cc737d0
libia <libia.roman@ucb.edu.bo>
2024-07-28 19:24:22 -0400

    again

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
+import React from 'react';
 
 function App() {
   return (
